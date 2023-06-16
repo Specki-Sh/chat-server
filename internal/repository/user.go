fc2b7a6bb4c99957c00da00332296f62873a4b57
Specki <yushakhzod@gmail.com>
2023-06-16 03:56:48 +0300

    init: implemented repository usecase for user

---
+++ internal/repository/user.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"chat-server/internal/domain/entity"
+	"database/sql"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db}
+}
+
+func (u UserRepository) CreateUser(user *entity.User) (*entity.User, error) {
+	var id int
+	query := "INSERT INTO users(username, password, email) VALUES ($1, $2, $3) returning id"
+	err := u.db.QueryRow(query, user.Username, user.Password, user.Email).Scan(&id)
+	if err != nil {
+		return &entity.User{}, err
+	}
+
+	user.ID = id
+	return user, nil
+}
+
+func (u UserRepository) GetUserByEmailAndPassword(email string, password string) (*entity.User, error) {
+	var user entity.User
+	query := "SELECT id, username, password, email FROM users WHERE email = $1 AND password = $2"
+	err := u.db.QueryRow(query, email, password).Scan(&user.ID, &user.Username, &user.Password, &user.Email)
+	if err != nil {
+		return &entity.User{}, err
+	}
+	return &user, nil
+}
