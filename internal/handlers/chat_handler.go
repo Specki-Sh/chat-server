887218a20a1c2fbc55266ca96a6ecab86c3ebd31
Specki <yushakhzod@gmail.com>
2023-06-23 13:48:47 +0300

    init: websocket chat handlers

---
+++ internal/handlers/chat_handler.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"chat-server/internal/domain/entity"
+	"chat-server/internal/service"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"nhooyr.io/websocket"
+	"strconv"
+	"sync"
+)
+
+type ChatHandler struct {
+	chatsMu sync.Mutex
+	chats   map[int]*service.Chat
+}
+
+func NewChatHandler() *ChatHandler {
+	return &ChatHandler{
+		chats: make(map[int]*service.Chat),
+	}
+}
+
+func (h *ChatHandler) JoinRoom(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	conn, err := websocket.Accept(c.Writer, c.Request, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close(websocket.StatusInternalError, "")
+
+	cl := &service.Client{
+		Conn:    conn,
+		Message: make(chan *entity.Message, 10),
+		RoomID:  id,
+	}
+	h.addClient(id, cl)
+	defer h.deleteClient(id, cl)
+
+	go cl.WriteMessage()
+
+	broadcast := make(chan *entity.Message, 10)
+	go cl.ReadMessage(broadcast)
+	h.broadcastManager(broadcast)
+
+}
+
+func (h *ChatHandler) broadcastManager(broadcast chan *entity.Message) {
+	for {
+		select {
+		case msg := <-broadcast:
+			if chat, ok := h.chats[msg.RoomID]; ok {
+				for cl := range chat.Clients {
+					cl.Message <- msg
+				}
+			}
+		}
+	}
+}
+
+func (h *ChatHandler) addClient(roomID int, c *service.Client) {
+	h.chatsMu.Lock()
+	defer h.chatsMu.Unlock()
+
+	chat, ok := h.chats[roomID]
+	if !ok {
+		chat = &service.Chat{
+			Clients: make(map[*service.Client]struct{}),
+		}
+		h.chats[roomID] = chat
+	}
+
+	chat.Clients[c] = struct{}{}
+}
+
+func (h *ChatHandler) deleteClient(roomId int, c *service.Client) {
+	h.chatsMu.Lock()
+	delete(h.chats[roomId].Clients, c)
+	h.chatsMu.Unlock()
+}
