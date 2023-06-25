3ae372336e19528bb14db0b6481989db92b30b7e
Specki <yushakhzod@gmail.com>
2023-06-25 19:58:16 +0300

    modify: added comment for helper methods

--- internal/handlers/chat_handler.go
+++ internal/handlers/chat_handler.go
@@ -26,12 +26,14 @@ func NewChatHandler() *ChatHandler {
 }
 
 func (h *ChatHandler) JoinRoom(c *gin.Context) {
+	// Get the chat ID from the "id" URL parameter.
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	// Accept the WebSocket connection.
 	conn, err := websocket.Accept(c.Writer, c.Request, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -39,22 +41,29 @@ func (h *ChatHandler) JoinRoom(c *gin.Context) {
 	}
 	defer conn.Close(websocket.StatusInternalError, "")
 
+	// Create a new client and add it to the chat.
 	cl := service.NewClient(conn, h.messageBuffSize, id)
 	h.addClient(id, cl)
 	defer h.deleteClient(id, cl)
 
+	// Start a go-routine to send messages to the client.
 	go cl.WriteMessage()
 
+	// Start the broadcast manager for the chat (if it is not already started).
 	go h.startBroadcastManager(id)
+
+	// Read messages from the client and send them to the chat's broadcast channel.
 	cl.ReadMessage(h.chats[id].Broadcast)
 }
 
+// startBroadcastManager starts the broadcast manager for the chat with the specified ID (if it is not already started).
 func (h *ChatHandler) startBroadcastManager(roomID int) {
 	if chat, ok := h.chats[roomID]; ok && !chat.BroadcastManagerStatus {
 		h.broadcastManager(chat.Broadcast)
 	}
 }
 
+// broadcastManager handles messages from the broadcast channel and sends them to all clients in the chat.
 func (h *ChatHandler) broadcastManager(broadcast chan *entity.Message) {
 	for {
 		select {
@@ -68,6 +77,7 @@ func (h *ChatHandler) broadcastManager(broadcast chan *entity.Message) {
 	}
 }
 
+// addClient adds a client to the chat with the specified ID. If the chat does not exist, it is created.
 func (h *ChatHandler) addClient(roomID int, c *service.Client) {
 	h.chatsMu.Lock()
 	defer h.chatsMu.Unlock()
@@ -81,6 +91,7 @@ func (h *ChatHandler) addClient(roomID int, c *service.Client) {
 	chat.Clients[c] = struct{}{}
 }
 
+// deleteClient removes a client from the chat with the specified ID. If there are no more clients in the chat, then the chat is deleted.
 func (h *ChatHandler) deleteClient(roomId int, c *service.Client) {
 	h.chatsMu.Lock()
 	delete(h.chats[roomId].Clients, c)
