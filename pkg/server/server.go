3f5d3011fc367fe3164474a01c59af159792dfe6
Specki <yushakhzod@gmail.com>
2023-06-26 13:12:32 +0300

    modify: deleted router to command file

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,38 +1,20 @@
 package server
 
 import (
-	"chat-server/internal/handlers"
 	"context"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
 )
 
-func NewServer(authHandler *handlers.AuthHandler, roomHandler *handlers.RoomHandler, chatHandler *handlers.ChatHandler) *Server {
-	return &Server{
-		httpServer:  &http.Server{},
-		route:       gin.New(),
-		authHandler: authHandler,
-		roomHandler: roomHandler,
-		chatHandler: chatHandler,
-	}
-}
-
 type Server struct {
 	httpServer *http.Server
-	route      *gin.Engine
-
-	authHandler *handlers.AuthHandler
-	roomHandler *handlers.RoomHandler
-	chatHandler *handlers.ChatHandler
 }
 
-func (s *Server) Run(port string) error {
-	s.setupRouter()
+func (s *Server) Run(port string, route http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
 		MaxHeaderBytes: 1 << 20,
-		Handler:        s.route,
+		Handler:        route,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
@@ -42,24 +24,4 @@ func (s *Server) Run(port string) error {
 
 func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
-}
-
-// Я не уверен, что это должно быть здесь
-func (s *Server) setupRouter() {
-	// auth
-	auth := s.route.Group("/auth")
-	auth.POST("/sign-up", s.authHandler.SignUp)
-	auth.POST("/sign-in", s.authHandler.SignIn)
-	auth.GET("/logout", s.authHandler.Logout)
-
-	// room
-	room := s.route.Group("/rooms")
-	room.POST("/", s.roomHandler.PostRoom)
-	room.GET("/:id", s.roomHandler.GetRoom)
-	room.PATCH("/info", s.roomHandler.PatchRoomInfo)
-	room.POST("/:id", s.roomHandler.DeleteRoom)
-
-	// ws
-	ws := s.route.Group("/ws")
-	ws.GET("/ws/joinRoom/:id", s.chatHandler.JoinRoom)
 }
