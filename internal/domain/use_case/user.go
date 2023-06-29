b799426b04dc699c9c4a39330b4a993df6788d91
Specki <yushakhzod@gmail.com>
2023-06-29 21:05:25 +0300

    modify: added UserPermissionMiddlewareByParam

--- internal/domain/use_case/user.go
+++ internal/domain/use_case/user.go
@@ -8,4 +8,5 @@ type UserUseCase interface {
 	CreateUser(req *entity.CreateUserReq) (*entity.CreateUserRes, error)
 	GetByEmailAndPassword(email string, password string) (*entity.User, error)
 	UserExists(id int) (bool, error)
+	EditUserProfile(req *entity.EditProfileReq) (*entity.EditProfileRes, error)
 }
--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -74,6 +74,26 @@ func (a *AuthHandler) Logout(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "logout successful"})
 }
 
+func (a *AuthHandler) PatchUserProfile(c *gin.Context) {
+	var req entity.EditProfileReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		a.logger.Errorf("error binding JSON: %v", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := a.userUseCase.EditUserProfile(&req)
+	if err != nil {
+		a.logger.Errorf("error editing user profile: %v", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	a.logger.Infof("user %v changed profile %v", req.ID, req)
+	c.JSON(http.StatusOK, gin.H{"message": "logout successful"})
+	c.JSON(http.StatusOK, user)
+}
+
 func (a *AuthHandler) UserIdentity(c *gin.Context) {
 	cookie, err := c.Cookie("jwt")
 	if err != nil {
@@ -98,6 +118,31 @@ func (a *AuthHandler) UserIdentity(c *gin.Context) {
 	c.Set(userCtx, userID)
 	c.Set(usernameCtx, username)
 	a.logger.Infof("user identity set: %d %s", userID, username)
+}
+
+func (a *AuthHandler) UserPermissionMiddlewareByParam(paramKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID, err := strconv.Atoi(c.Param(paramKey))
+		if err != nil {
+			a.logger.Errorf("Error converting room ID to int: %v", err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid user ID"})
+			return
+		}
+
+		userTokenID, err := getUserID(c)
+		if err != nil {
+			a.logger.Errorf("\"Error getting user ID: %v", err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if userID != userTokenID {
+			a.logger.Infof("User with ID: %v has not permission of user with ID: %v", userTokenID, userID)
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "access denied"})
+			return
+		}
+		c.Next()
+	}
 }
 
 func (a *AuthHandler) UserExistMiddlewareByParam(paramKey string) gin.HandlerFunc {
